c335d8f5690aae308998ddb48945e3fdd77a6fbc
Anders Jorgensen <abrj@itu.dk>
2012-10-30 15:07:28 +0100

    Added the files

---
+++ TCP/TCP/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TCP
+{
+    class Client
+    {
+        public static void Run()
+        {
+            int serverPort = 8080;
+            IPAddress serverIp = IPAddress.Parse("10.25.232.214"); //IPAdress
+            Console.WriteLine("Server IP: {0}", serverIp);
+
+            Socket echoSocket = new Socket(
+                serverIp.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+            echoSocket.Connect(serverIp, serverPort);
+
+            using (Stream echoServiceStream = new NetworkStream(echoSocket, true))
+
+            {
+                string msg = "Hello world";
+                Byte[] msgByte = encodeString(msg);
+                //Gets the length of the message in bytes
+                Byte[] msgByteLength = BitConverter.GetBytes(msg.Length);
+                //Sends the length of the message in bytes
+                echoServiceStream.Write(msgByteLength, 0, msgByteLength.Length);
+                echoServiceStream.Flush();
+                //Sends the message
+                echoServiceStream.Write(msgByte, 0, msgByte.Length);
+                //writer.Write(msg.Length);
+               // writer.WriteLine(msg);
+                echoServiceStream.Flush();
+             
+            }
+        }
+
+        private static Byte[] encodeString(string s)
+        {
+            System.Text.ASCIIEncoding enconding = new System.Text.ASCIIEncoding();
+            Byte[] message = enconding.GetBytes(s);
+            return message;
+        }
+
+      
+    }
+}
---
+++ TCP/TCP/Server.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TCP
+{
+    class Server
+    {
+        public static void Run()
+        {
+            int serverPort = 8080;
+            using (Socket echoListener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp))
+            {
+                IPEndPoint echoEndPoint = new IPEndPoint(IPAddress.Any, serverPort);
+                echoListener.Bind(echoEndPoint);
+                echoListener.Listen(20);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for connections...");
+                    Socket incomingConnection = echoListener.Accept();
+
+                    using (NetworkStream connStream = new NetworkStream(incomingConnection, true))
+                    {
+                        Console.WriteLine("Accepted connection");
+                        //A buffer to hold the size of the message
+                        byte[] buffer = new byte[4];
+                        //Read the size of the message
+                        connStream.Read(buffer, 0, 4);
+
+                        //Convert the byte[] to an int
+                        int msgLength = (buffer[0]) + (buffer[1] << 8) + (buffer[2] << 16) + (buffer[3] << 24);
+
+                        //A buffer to hold the message
+                        buffer = new byte[msgLength];
+                        //Read the message
+                        connStream.Read(buffer, 0, msgLength);
+
+                        //Decode the message to a string
+                        String msg = decode(buffer);
+
+                        Console.WriteLine("[Server Recieved]: {0}", msg);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decode a byte[] into a String
+        /// </summary>
+        /// <param name="arr">The byte[] to decode</param>
+        /// <returns>The decoded String</returns>
+        private static String decode(byte[] arr)
+        {
+            System.Text.ASCIIEncoding decoder = new System.Text.ASCIIEncoding();
+            String message = decoder.GetString(arr);
+            return message;
+        }
+    }
+}
